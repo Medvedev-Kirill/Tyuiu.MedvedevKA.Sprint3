27440040c5f8912c64648f7f7d3f27d5b658d3a2
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-10-16 14:21:34 +0500

    Add project files.

---
+++ Tyuiu.MedvedevKA.Sprint3.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MedvedevKA.Sprint3.Task0.V1.Lib;
+
+namespace Tyuiu.MedvedevKA.Sprint3.Task0.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double res = ds.GetSumSeries(x);
+            double wait = 1935;
+
+            Assert.AreEqual(res,wait);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint3.Task0.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevKA.Sprint3.Task0.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevKA.Sprint3.Task0.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fbf01c2-4766-4e1e-88af-49e27220a032")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
